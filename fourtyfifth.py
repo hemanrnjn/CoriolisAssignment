b96839a178c4f78ef0333e05f88c2cb935e397b9
hemanrnjn <himanshurnjn04@gmail.com>
2018-09-10 03:00:26 +0530

    minor changes

--- fourtyfifth.py
+++ fourtyfifth.py
@@ -1,14 +1,32 @@
 import random
 
 
-def child_word_game(file_name):
-    file = open(file_name, 'r')
-    words = file.read().split()
-    start = random.choice(words)
-    prev = start
+def find_word(prev):
+    found = '',
+    f = 0
     for word in words:
-        if word[]
+        if prev[-1] == word[0]:
+            found = word
+            f = 1
+            words.remove(word)
+            break
+    return f, found
 
 
 file_name = input('Enter File Name\n')
-child_word_game(file_name)
+file = open(file_name, 'r')
+words = file.read().split()
+start = random.choice(words)
+print('Child1: ', start)
+res = find_word(start)
+print('Child2: ', res[1])
+x = 0
+while res[0] == 1:
+    start = res[1]
+    res = find_word(start)
+    if x == 0:
+        print('Child1 :', res[1])
+        x = 1
+    else:
+        print('Child2 :', res[1])
+        x = 0
